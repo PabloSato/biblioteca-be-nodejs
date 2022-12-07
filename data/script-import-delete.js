08f0654191ea9410f43428fab44ce13e96b704f1
PabloSato <pablofersato@gmail.com>
2022-12-07 14:49:19 +0100

    add dev-data authors

--- data/script-import-delete.js
+++ data/script-import-delete.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 
 const Book = require('./../models/bookModel');
 const Tag = require('./../models/tagModel');
+const Atuhor = require('./../models/authorModel');
+const Author = require('./../models/authorModel');
 
 dotenv.config({ path: './config.env' });
 // --------------------------------------------- DB ----------------------------------------
@@ -16,14 +18,19 @@ mongoose
 // --------------------------------------------- 1 - READ JSON FILES ----------------------------------------
 const books = JSON.parse(
   // fs.readFileSync(`${__dirname}/simple-booksv1.json`, 'utf-8') // => Simple Book (all string)
-  fs.readFileSync(`${__dirname}/simple-booksv2.json`, 'utf-8') // => Books with Tags IDs
+  // fs.readFileSync(`${__dirname}/simple-booksv2.json`, 'utf-8') // => Books with Tags IDs
+  fs.readFileSync(`${__dirname}/simple-booksv3.json`, 'utf-8') // => Books with Tags IDs and Authors IDs
 );
 const tags = JSON.parse(fs.readFileSync(`${__dirname}/tags.json`, 'utf-8'));
+const authors = JSON.parse(
+  fs.readFileSync(`${__dirname}/authors.json`, 'utf-8')
+);
 // --------------------------------------------- 2 - IMPORT TO DB ----------------------------------------
 const importData = async () => {
   try {
-    await Book.create(books);
     await Tag.create(tags);
+    await Author.create(authors);
+    await Book.create(books);
     console.log('DATA LOADED!');
   } catch (err) {
     console.log(err);
@@ -35,6 +42,7 @@ const deleteData = async () => {
   try {
     await Book.deleteMany();
     await Tag.deleteMany();
+    await Author.deleteMany();
     console.log('DATA DELETED!');
   } catch (err) {
     console.log(err);
