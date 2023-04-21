a96daeebcdd51fdfd9c4a3e6cc6885edea516769
Pablo Fernández Sato <pablofersato@gmail.com>
2023-04-21 16:52:10 +0200

    add data

---
+++ utils/uploadImage.js
@@ -0,0 +1,10 @@
+const sharp = require('sharp');
+const crypto = require('crypto');
+const path = require('path');
+const fs = require('fs');
+
+const { uploadImage } = require('./../middleware/upload');
+
+module.exports = async (req, res) => {
+  let result = {};
+};
